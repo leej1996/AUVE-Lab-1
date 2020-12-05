b9d6da9a7509b295f70578abc854b80af9f419e2
imstevenpm.study <imstevenpm.study@gmail.com>
2020-12-05 20:58:10 +0100

    directories, images, report, some changes in code structure

--- MATLAB/Question1.m
+++ MATLAB/Question1.m
@@ -1,17 +1,36 @@
-%% Question 1.1
-m = 0.95;
-nb = 16;
-p = 0.5;
-sigma = sqrt(1 - (m^2));
+%% Main function for the questions of the first part
+function [] = Question1()
+  
+  % Solves Question 1.1
+  Question11();
+  
+  % Solves Questions 1.2
+  Question12();
+  
+end
 
-for N = [100 4000]
-    X_n = randn(N,1);
-    X_u = 2*sqrt(3)*(rand(N,1)-0.5);
-    X_s = randn(N,1)*sqrt(1-m*m)+m;
+%% Solves Questions 1.1
+function [] = Question11()
+  
+  % Parameters
+  m = 0.95; % means
+  nb = 16; % number of bars
+  p = 0.5; % prob
+  sigma = sqrt(1 - (m^2)); % variance
+  
+  % For N=100 and N=4000 realizations
+  for N = [100 4000]
+    
+    % Distributions
+    X_n = randn(N,1); % X normally distributed
+    X_u = 2*sqrt(3)*(rand(N,1)-0.5); % X uniformly distributed
+    
+    % X driven by a Gaussian mixture
+    X_s = randn(N,1)*sqrt(1-m*m)+m; 
     k = find(rand(N,1)>0.5);
     X_s(k) = X_s(k) -2*m;
     
-    %Plot normal distribution
+    % Plot normal distribution
     [n,b] = hist(X_n,nb);
     figure;
     hold on;
@@ -44,7 +63,7 @@ for N = [100 4000]
     figure;
     plot(1:N,X_u,'*');
     title(['Uniform Independent Random Signal, N = ',num2str(N)]);
-        
+    
     %Plot Symmetric Gaussian Mixture
     [n,b] = hist(X_s,nb);
     figure;
@@ -63,31 +82,41 @@ for N = [100 4000]
     figure;
     plot(1:N,X_s,'*');
     title(['Symmetric Independent Random Signal, N = ',num2str(N)]);
+  end
+  
+  
+  
 end
 
-%% Question 1.2
-sigma1 = 2;
-sigma2 = 5;
-p = 0.9;
 
-C = [sigma1^2, sigma1*sigma2*p;sigma1*sigma2*p, sigma2^2];
-
-x = chol(C,'lower')*randn(2,200);
-
-m = mean(x,2);
-covariance = cov(x');
-disp("The mean is: ");
-disp(m);
-disp("The covariance is: ");
-disp(covariance);
-
-figure;
-hold on;
-plot(x(1,:), x(2,:), '*');
-
-P0 = 0.91;
-theta = linspace(0,2*pi,100);
-X = sqrt(-2*log(1-P0))*chol(C,'lower')*[cos(theta);sin(theta)] + m*ones(1,length(theta));
-
-plot(X(1,:),X(2,:));
-title('Joint Distribution with 91% Confidence Ellipse');
+function [] = Question12()
+  
+  
+  %% Question 1.2
+  sigma1 = 2;
+  sigma2 = 5;
+  p = 0.9;
+  
+  C = [sigma1^2, sigma1*sigma2*p;sigma1*sigma2*p, sigma2^2];
+  
+  x = chol(C,'lower')*randn(2,200);
+  
+  m = mean(x,2);
+  covariance = cov(x');
+  disp("The mean is: ");
+  disp(m);
+  disp("The covariance is: ");
+  disp(covariance);
+  
+  figure;
+  hold on;
+  plot(x(1,:), x(2,:), '*');
+  
+  P0 = 0.91;
+  theta = linspace(0,2*pi,100);
+  X = sqrt(-2*log(1-P0))*chol(C,'lower')*[cos(theta);sin(theta)] + m*ones(1,length(theta));
+  
+  plot(X(1,:),X(2,:));
+  title('Joint Distribution with 91% Confidence Ellipse');
+  
+  end
